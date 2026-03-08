656ee545e79cb9725a2133dabb3bf80e3685839e
rahmadahyaWeb <thelastpc24@gmail.com>
2026-03-09 00:15:37 +0800

    fix

--- database/migrations/2026_03_05_032347_create_invoices_table.php
+++ database/migrations/2026_03_05_032347_create_invoices_table.php
@@ -14,7 +14,11 @@ return new class extends Migration
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
             $table->foreignId('business_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('customer_id')->constrained()->nullOnDelete();
+
+            $table->foreignId('customer_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
 
             $table->string('invoice_number');
             $table->string('public_token', 60)->unique();
